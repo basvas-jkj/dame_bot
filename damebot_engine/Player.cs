b7ddc937fb6db8aaadb461f8a6743db0c3625fc7
Václav Stupka <vasstupka@seznam.cz>
2025-05-15 21:13:55 +0200

    Správa kamenů přesunuta do třídy Player.

--- damebot_engine/Player.cs
+++ damebot_engine/Player.cs
@@ -1,7 +1,36 @@
-﻿namespace damebot_engine
+﻿using System.Collections.Generic;
+
+namespace damebot_engine
 {
 	public abstract class Player
-	{ }
+	{
+		protected List<Piece> pieces = new();
+
+		public IReadOnlyList<Piece> GetPieces()
+		{
+			return pieces;
+		}
+		public void AddPiece(Piece p)
+		{
+			pieces.Add(p);
+		}
+		public void RemovePiece(Piece p)
+		{
+			pieces.Remove(p);
+		}
+
+		public bool CanCapture()
+		{
+			foreach (Piece p in pieces)
+			{
+				if (p.CanCapture())
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
 	public class RealPlayer: Player
 	{ }
 	public class AutomaticPlayer: Player
