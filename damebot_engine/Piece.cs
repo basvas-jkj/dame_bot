3b7f6e99784747464208cfb11990bbd585412fd7
Václav Stupka <vasstupka@seznam.cz>
2025-05-15 10:23:56 +0200

    Implementace pro SQUARE a Piece.

--- damebot_engine/Piece.cs
+++ damebot_engine/Piece.cs
@@ -1,26 +1,106 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace damebot_engine
 {
-	public abstract class Piece(Square position, Image image)
+	using SQUARE_DIFF = (int x, int y);
+
+	public enum MOVE_TYPE
 	{
-		public Square position { get; private set; } = position;
-		public Image image { get; } = image;
+		move, jump, invalid
 	}
-	class WhiteMan(Square position): Piece(position, loaded_image)
+	public abstract class Piece(SQUARE position, Image image)
+	{
+		public Image image { get; } = image;
+		public SQUARE position { get; private set; } = position;
+
+		public void Move(SQUARE next_position)
+		{
+			position = next_position;
+		}
+		public abstract MOVE_TYPE GetMoveType(SQUARE original, SQUARE next);
+
+		protected static SQUARE_DIFF Normalise(SQUARE_DIFF diff)
+		{
+			return (Math.Sign(diff.x), Math.Sign(diff.y));
+		}
+	}
+	class WhiteMan(SQUARE position): Piece(position, loaded_image)
 	{
 		static Image loaded_image = Image.FromFile("img/white_man.png");
+
+		public override MOVE_TYPE GetMoveType(SQUARE original, SQUARE next)
+		{
+			SQUARE_DIFF difference = next - original;
+			if (difference == (1, 1) || difference == (-1, 1))
+			{
+				return MOVE_TYPE.move;
+			}
+			else if (difference == (2, 2) || difference == (-2, 2))
+			{
+				return MOVE_TYPE.jump;
+			}
+			else
+			{
+				return MOVE_TYPE.invalid;
+			}
+		}
 	}
-	class BlackMan(Square position): Piece(position, loaded_image)
+	class BlackMan(SQUARE position): Piece(position, loaded_image)
 	{
 		static Image loaded_image = Image.FromFile("img/black_man.png");
+
+		public override MOVE_TYPE GetMoveType(SQUARE original, SQUARE next)
+		{
+			SQUARE_DIFF difference = next - original;
+			if (difference == (1, -1) || difference == (-1, -1))
+			{
+				return MOVE_TYPE.move;
+			}
+			else if (difference == (2, -2) || difference == (-2, -2))
+			{
+				return MOVE_TYPE.jump;
+			}
+			else
+			{
+				return MOVE_TYPE.invalid;
+			}
+		}
 	}
-	class WhiteKing(Square position): Piece(position, loaded_image)
+	class WhiteKing(SQUARE position): Piece(position, loaded_image)
 	{
 		static Image loaded_image = Image.FromFile("img/white_king.png");
+
+		public override MOVE_TYPE GetMoveType(SQUARE original, SQUARE next)
+		{
+			SQUARE_DIFF difference = next - original;
+			SQUARE_DIFF direction = Normalise(difference);
+
+			if (difference.x != difference.y && difference.x != -difference.y)
+			{
+				return MOVE_TYPE.invalid;
+			}
+
+#warning Doplnit implementaci.
+			return MOVE_TYPE.move;
+		}
 	}
-	class BlackKing(Square position): Piece(position, loaded_image)
+	class BlackKing(SQUARE position): Piece(position, loaded_image)
 	{
 		static Image loaded_image = Image.FromFile("img/black_king.png");
+
+		public override MOVE_TYPE GetMoveType(SQUARE original, SQUARE next)
+		{
+			SQUARE_DIFF difference = next - original;
+			SQUARE_DIFF direction = Normalise(difference);
+
+			if (difference.x != difference.y && difference.x != -difference.y)
+			{
+				return MOVE_TYPE.invalid;
+			}
+
+#warning Doplnit implementaci.
+			return MOVE_TYPE.move;
+		}
 	}
 }
---
+++ damebot_engine/SQUARE.cs
@@ -0,0 +1,21 @@
+﻿namespace damebot_engine
+{
+	public readonly struct SQUARE(int x, int y)
+	{
+		public int X { get; } = x;
+		public int Y { get; } = y;
+		public bool IsOnBoard(IBoard board)
+		{
+			return 0 <= X && X < board.Size && 0 <= Y && Y < board.Size;
+		}
+
+		public static (int x, int y) operator -(SQUARE a, SQUARE b)
+		{
+			return (a.X - b.X, a.Y - b.Y);
+		}
+		public static SQUARE operator +(SQUARE a, (int x, int y) b)
+		{
+			return new SQUARE(a.X + b.x, a.Y + b.y);
+		}
+	}
+}
