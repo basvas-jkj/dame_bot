f940f32d7014442b6ae02d686d0576f5ad0cc8aa
Václav Stupka <vasstupka@seznam.cz>
2025-05-21 09:40:17 +0200

    Engine olhásí počítá s možností, že není nalezen žádný tah.

--- damebot_engine/DameEngine.cs
+++ damebot_engine/DameEngine.cs
@@ -3,110 +3,127 @@ using System.Threading.Tasks;
 
 namespace damebot_engine
 {
-	public delegate void MoveEvent(IPlayer? next_player);
-	public delegate void MarkEvent(MOVE m);
-	public interface IEngine
-	{
-		bool IsOnMovePiece(SQUARE position);
-		MOVE_INFO ValidateMove(Piece piece, MOVE m, SQUARE next);
-		void PerformMove(MOVE m);
+    public delegate void MoveEvent(IPlayer? next_player);
+    public delegate void MarkEvent(MOVE m);
+    public delegate void GameOverEvent(IPlayer p);
+    public interface IEngine
+    {
+        bool IsOnMovePiece(SQUARE position);
+        MOVE_INFO ValidateMove(Piece piece, MOVE m, SQUARE next);
+        void PerformMove(MOVE m);
 
-		event MoveEvent? OnMove;
-		event MarkEvent? OnMark;
-	}
-	public class DameEngine(IBoard board, IPlayer white, IPlayer black): IEngine
-	{
-		public IBoard Board { get; } = board;
-		public IPlayer White { get; } = white;
-		public IPlayer Black { get; } = black;
+        event MoveEvent? OnMove;
+        event MarkEvent? OnMark;
+        event GameOverEvent? OnGameOver;
+    }
+    public class DameEngine(IBoard board, IPlayer white, IPlayer black): IEngine
+    {
+        public IBoard Board { get; } = board;
+        public IPlayer White { get; } = white;
+        public IPlayer Black { get; } = black;
 
-		private IPlayer player_on_move = white;
-		private IPlayer waiting_player
-		{
-			get => (player_on_move == White) ? Black : White;
-		}
-		private void SwitchPlayers()
-		{
-			player_on_move = (player_on_move == White) ? Black : White;
-		}
+        private IPlayer player_on_move = white;
+        private IPlayer waiting_player
+        {
+            get => (player_on_move == White) ? Black : White;
+        }
+        private void SwitchPlayers()
+        {
+            player_on_move = (player_on_move == White) ? Black : White;
+        }
 
-		public event MoveEvent? OnMove;
-		public event MarkEvent? OnMark;
+        public event MoveEvent? OnMove;
+        public event MarkEvent? OnMark;
+        public event GameOverEvent? OnGameOver;
 
-		public bool IsOnMovePiece(SQUARE position)
-		{
-			if (player_on_move == White)
-			{
-				return Board[position] is WhiteMan || Board[position] is WhiteKing;
-			}
-			else
-			{
-				return Board[position] is BlackMan || Board[position] is BlackKing;
-			}
-		}
-		public MOVE_INFO ValidateMove(Piece piece, MOVE m, SQUARE next)
-		{
-			MOVE_INFO info = piece.GetMoveInfo(Board, m, next);
+        public bool IsOnMovePiece(SQUARE position)
+        {
+            if (player_on_move == White)
+            {
+                return Board[position] is WhiteMan || Board[position] is WhiteKing;
+            }
+            else
+            {
+                return Board[position] is BlackMan || Board[position] is BlackKing;
+            }
+        }
+        public MOVE_INFO ValidateMove(Piece piece, MOVE m, SQUARE next)
+        {
+            MOVE_INFO info = piece.GetMoveInfo(Board, m, next);
 
-			if (info.CompleteJump || info.IncompleteJump)
-			{
-				return info;
-			}
-			if (info.Move && !player_on_move.CanCapture(Board))
-			{
-				return info;
-			}
-			else
-			{
-				return new MOVE_INFO();
-			}
-		}
+            if (info.CompleteJump || info.IncompleteJump)
+            {
+                return info;
+            }
+            if (info.Move && !player_on_move.CanCapture(Board))
+            {
+                return info;
+            }
+            else
+            {
+                return new MOVE_INFO();
+            }
+        }
 
-		public void PerformMove(MOVE m)
-		{
-			Debug.Assert(Board[m.Squares[0]] != null);
+        public void PerformMove(MOVE m)
+        {
+            Debug.Assert(Board[m.Squares[0]] != null);
 
-			Piece moving = Board[m.Squares[0]]!;
-			Piece moved = Board.PerformMove(m);
+            Piece moving = Board[m.Squares[0]]!;
+            Piece moved = Board.PerformMove(m);
 
-			foreach (Piece captured in m.CapturedPieces)
-			{
-				waiting_player.RemovePiece(captured);
-				Board.RemovePiece(captured);
-			}
+            foreach (Piece captured in m.CapturedPieces)
+            {
+                waiting_player.RemovePiece(captured);
+                Board.RemovePiece(captured);
+            }
 
-			if (moved.CanBePromoted(Board))
-			{
-				Board.RemovePiece(moved);
-				moved = moved.Promote();
-				Board.AddPiece(moved);
-			}
+            if (moved.CanBePromoted(Board))
+            {
+                Board.RemovePiece(moved);
+                moved = moved.Promote();
+                Board.AddPiece(moved);
+            }
 
-			player_on_move.RemovePiece(moving);
-			player_on_move.AddPiece(moved);
+            player_on_move.RemovePiece(moving);
+            player_on_move.AddPiece(moved);
 
-			SwitchPlayers();
+            SwitchPlayers();
 
-			if (player_on_move.Automatic)
-			{
-				OnMove?.Invoke(null);
+            if (player_on_move.GetPieces().Count == 0)
+            {
+                OnGameOver?.Invoke(waiting_player);
+            }
+            else if (player_on_move.Automatic)
+            {
+                OnMove?.Invoke(null);
 
-				var v = TaskScheduler.FromCurrentSynchronizationContext();
-				player_on_move.FindNextMove(Board, waiting_player)
-					.ContinueWith(PerformAutomaticMove);
+                var v = TaskScheduler.FromCurrentSynchronizationContext();
+                player_on_move.FindNextMove(Board, waiting_player)
+                    .ContinueWith(PerformAutomaticMove);
+            }
+            else
+            {
+                OnMove?.Invoke(player_on_move);
+                OnMark?.Invoke(new MOVE());
+            }
 
-			}
-			else
-			{
-				OnMove?.Invoke(player_on_move);
-				OnMark?.Invoke(new MOVE());
-			}
-
-			void PerformAutomaticMove(Task<MOVE> generated)
-			{
-				PerformMove(generated.Result);
-				OnMark?.Invoke(generated.Result);
-			}
-		}
-	}
+            void PerformAutomaticMove(Task<MOVE?> generated)
+            {
+                if (generated.Exception != null)
+                {
+                    Debug.WriteLine(generated.Exception);
+                }
+                else if (generated.Result != null)
+                {
+                    PerformMove(generated.Result.Value);
+                    OnMark?.Invoke(generated.Result.Value);
+                }
+                else
+                {
+                    OnGameOver?.Invoke(waiting_player);
+                }
+            }
+        }
+    }
 }
--- damebot_engine/Player.cs
+++ damebot_engine/Player.cs
@@ -1,170 +1,179 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace damebot_engine
 {
-	using EVALUATED_MOVE = (MOVE move, int evaluation);
-	using MOVE_TASK = (MOVE move, Task<int> evaluation_task);
+    using EVALUATED_MOVE = (MOVE? move, int evaluation);
+    using MOVE_TASK = (MOVE move, Task<int> evaluation_task);
 
-	public enum PLAYER_TYPE { min, max }
-	public interface IPlayer
-	{
-		bool Automatic { get; }
+    public enum PLAYER_TYPE { min, max }
+    public interface IPlayer
+    {
+        bool Automatic { get; }
 
-		void AddPiece(Piece p);
-		void RemovePiece(Piece p);
-		IPlayer Copy();
+        void AddPiece(Piece p);
+        void RemovePiece(Piece p);
+        IPlayer Copy();
 
-		bool CanCapture(IBoard board);
-		Task<MOVE> FindNextMove(IBoard board, IPlayer other);
-		IReadOnlyList<Piece> GetPieces();
-	}
-	public class Player(bool automatic, PLAYER_TYPE type): IPlayer
-	{
-		public bool Automatic { get; } = automatic;
-		readonly PLAYER_TYPE type = type;
-		List<Piece> pieces = new();
+        bool CanCapture(IBoard board);
+        Task<MOVE?> FindNextMove(IBoard board, IPlayer other);
+        IReadOnlyList<Piece> GetPieces();
+    }
+    public class Player(bool automatic, PLAYER_TYPE type): IPlayer
+    {
+        public bool Automatic { get; } = automatic;
+        readonly PLAYER_TYPE type = type;
+        List<Piece> pieces = new();
 
-		public IReadOnlyList<Piece> GetPieces()
-		{
-			return pieces;
-		}
-		public void AddPiece(Piece p)
-		{
-			pieces.Add(p);
-		}
-		public void RemovePiece(Piece p)
-		{
-			pieces.Remove(p);
-		}
+        public IReadOnlyList<Piece> GetPieces()
+        {
+            return pieces;
+        }
+        public void AddPiece(Piece p)
+        {
+            pieces.Add(p);
+        }
+        public void RemovePiece(Piece p)
+        {
+            pieces.Remove(p);
+        }
 
-		public IPlayer Copy()
-		{
-			return new Player(Automatic, type)
-			{
-				pieces = new List<Piece>(pieces)
-			};
-		}
+        public IPlayer Copy()
+        {
+            return new Player(Automatic, type)
+            {
+                pieces = new List<Piece>(pieces)
+            };
+        }
 
-		int InitialEvaluation()
-		{
-			return (type == PLAYER_TYPE.min) ? 1000 : -1000;
-		}
-		int CompareEvaluations(int original_evaluation, int new_evaluation)
-		{
-			if (type == PLAYER_TYPE.min)
-			{
-				return original_evaluation - new_evaluation;
-			}
-			else
-			{
-				return new_evaluation - original_evaluation;
-			}
-		}
-		IEnumerable<MOVE> EnumerateAllMoves(IBoard board)
-		{
-			foreach (Piece p in pieces)
-			{
-				foreach (MOVE m in p.EnumerateMoves(board, new MOVE(p.Position)))
-				{
-					yield return m;
-				}
-			}
-		}
-		IEnumerable<MOVE> EnumerateAllJumps(IBoard board)
-		{
-			foreach (Piece p in pieces)
-			{
-				foreach (MOVE m in p.EnumerateJumps(board, new MOVE(p.Position)))
-				{
-					yield return m;
-				}
-			}
-		}
-		public bool CanCapture(IBoard board)
-		{
-			foreach (Piece p in pieces)
-			{
-				if (p.CanCapture(board))
-				{
-					return true;
-				}
-			}
-			return false;
-		}
-		public async Task<MOVE> FindNextMove(IBoard board, IPlayer other)
-		{
-			Debug.WriteLine("======================================");
-			EVALUATED_MOVE evaluation = await FindNextMove(board, other, 1);
-			return evaluation.move;
-		}
+        const int MinInitialEvaluation = 999_999;
+        const int MaxInitialEvaluation = -999_999;
+        int InitialEvaluation
+        {
+            get => (type == PLAYER_TYPE.min) ? MinInitialEvaluation : MaxInitialEvaluation;
+        }
+        int CompareEvaluations(int original_evaluation, int new_evaluation)
+        {
+            if (type == PLAYER_TYPE.min)
+            {
+                return original_evaluation - new_evaluation;
+            }
+            else
+            {
+                return new_evaluation - original_evaluation;
+            }
+        }
+        IEnumerable<MOVE> EnumerateAllMoves(IBoard board)
+        {
+            foreach (Piece p in pieces)
+            {
+                foreach (MOVE m in p.EnumerateMoves(board, new MOVE(p.Position)))
+                {
+                    yield return m;
+                }
+            }
+        }
+        IEnumerable<MOVE> EnumerateAllJumps(IBoard board)
+        {
+            foreach (Piece p in pieces)
+            {
+                foreach (MOVE m in p.EnumerateJumps(board, new MOVE(p.Position)))
+                {
+                    yield return m;
+                }
+            }
+        }
+        public bool CanCapture(IBoard board)
+        {
+            foreach (Piece p in pieces)
+            {
+                if (p.CanCapture(board))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public async Task<MOVE?> FindNextMove(IBoard board, IPlayer other)
+        {
+            //Debug.WriteLine("======================================");
+            EVALUATED_MOVE evaluation = await FindNextMove(board, other, 1);
+            return evaluation.move;
+        }
 
-		const int max_depth = 2;
-		async public Task<EVALUATED_MOVE> FindNextMove(IBoard board, IPlayer other, int depth)
-		{
-			List<MOVE_TASK> tasks = new();
-			IEnumerable<MOVE> moves = CanCapture(board) ? EnumerateAllJumps(board) : EnumerateAllMoves(board);
+        const int max_depth = 2;
+        async public Task<EVALUATED_MOVE> FindNextMove(IBoard board, IPlayer other, int depth)
+        {
+            List<MOVE_TASK> tasks = new();
+            IEnumerable<MOVE> moves = CanCapture(board) ? EnumerateAllJumps(board) : EnumerateAllMoves(board);
 
-			foreach (MOVE m in moves)
-			{
-				Task<int> t = EvaluateMove(board, other, depth, m);
-				tasks.Add((m, t));
-			}
+            foreach (MOVE m in moves)
+            {
+                Task<int> t = EvaluateMove(board, other, depth, m);
+                tasks.Add((m, t));
+            }
 
-			int best_evaluation = InitialEvaluation();
-			List<MOVE> best_moves = new();
-			foreach ((MOVE move, Task<int> evaluation_task) in tasks)
-			{
-				int evaluation = await evaluation_task;
-				int comparison = CompareEvaluations(best_evaluation, evaluation);
+            int best_evaluation = InitialEvaluation;
+            List<MOVE> best_moves = new();
+            foreach ((MOVE move, Task<int> evaluation_task) in tasks)
+            {
+                int evaluation = await evaluation_task;
+                int comparison = CompareEvaluations(best_evaluation, evaluation);
 
-				if (comparison < 0)
-				{
-					continue;
-				}
-				else if (comparison > 0)
-				{
-					best_evaluation = evaluation;
-					best_moves = [move];
-				}
-				else
-				{
-					best_moves.Add(move);
-				}
-			}
+                if (comparison < 0)
+                {
+                    continue;
+                }
+                else if (comparison > 0)
+                {
+                    best_evaluation = evaluation;
+                    best_moves = [move];
+                }
+                else
+                {
+                    best_moves.Add(move);
+                }
+            }
 
-			Random generator = new();
-			int random = generator.Next(best_moves.Count);
-			return (best_moves[random], best_evaluation);
-		}
-		async Task<int> EvaluateMove(IBoard board, IPlayer other, int depth, MOVE m)
-		{
-			Piece moving = board[m.Squares[0]]!;
-			(IBoard simulated, Piece moved) = board.SimulateMove(m);
-			Debug.WriteLine(depth);
-			Debug.WriteLine(simulated);
+            Random generator = new();
+            int random = generator.Next(best_moves.Count);
 
-			if (depth == max_depth)
-			{
-				return simulated.EvaluatePosition();
-			}
-			else
-			{
-				Player me = (Player)Copy();
-				me.RemovePiece(moving);
-				me.AddPiece(moved);
+            if (random < 0 || random >= best_moves.Count)
+            {
+                return (null, InitialEvaluation);
+            }
+            else
+            {
+                return (best_moves[random], best_evaluation);
+            }
+        }
+        async Task<int> EvaluateMove(IBoard board, IPlayer other, int depth, MOVE m)
+        {
+            Piece moving = board[m.Squares[0]]!;
+            (IBoard simulated, Piece moved) = board.SimulateMove(m);
+            //Debug.WriteLine(depth);
+            //Debug.WriteLine(simulated);
 
-				Player you = (Player)other.Copy();
-				foreach (Piece captured in m.CapturedPieces)
-				{
-					you.RemovePiece(captured);
-				}
+            if (depth == max_depth)
+            {
+                return simulated.EvaluatePosition();
+            }
+            else
+            {
+                Player me = (Player)Copy();
+                me.RemovePiece(moving);
+                me.AddPiece(moved);
 
-				EVALUATED_MOVE move = await you.FindNextMove(simulated, me, depth + 1);
-				return move.evaluation;
-			}
-		}
-	}
+                Player you = (Player)other.Copy();
+                foreach (Piece captured in m.CapturedPieces)
+                {
+                    you.RemovePiece(captured);
+                }
+
+                EVALUATED_MOVE move = await you.FindNextMove(simulated, me, depth + 1);
+                return move.evaluation;
+            }
+        }
+    }
 }
